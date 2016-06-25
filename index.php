35fc4065bebfe316ab9bb047ae043f7993875a6b
Moustakime KIFIA <mkifia@icloud.com>
2016-06-25 14:11:36 +0200

    first step, install && include angular+bootstrap in index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>TP - Authors</title>
+	<script type="text/javascript" src="bower_components/angular/angular.min.js"></script>
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+	
+</body>
+</html>
